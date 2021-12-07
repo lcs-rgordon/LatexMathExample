22cefd0ca9638d580e809fb078dd77c3c6b3d741
Russell Gordon <rgordon@lcs.on.ca>
2021-12-07 11:30:03 -0500

    Initial Commit

---
+++ LatexMathExample/LatexMathExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  LatexMathExampleApp.swift
+//  LatexMathExample
+//
+//  Created by Russell Gordon on 2021-12-07.
+//
+
+import SwiftUI
+
+@main
+struct LatexMathExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
